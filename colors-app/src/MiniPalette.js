342973263e63c617c7e3dfbadcf2d0a72fc66fa5
Tariq <tariqshariff.ts@gmail.com>
2021-12-22 15:19:29 +0530

    start styling mini palette

---
+++ colors-app/src/MiniPalette.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  root: {
+    backgroundColor: "white",
+    borderRadius: "5px",
+    border: "1px solid black",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  colors: {
+    backgroundColor: "grey",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "0",
+    color: "black",
+    fontSize: "1rem",
+    position: "relative",
+  },
+  emoji: {
+    marginLeft: "0.5rem",
+  },
+};
+
+function MiniPalette(props) {
+  const { classes, paletteName, emoji, colors } = props;
+  return (
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
+    </div>
+  );
+}
+export default withStyles(styles)(MiniPalette);
--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import MiniPalette from "./MiniPalette";
 export default class paletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
       <div>
         {palettes.map((palette) => (
-          <Link to={`palette/${palette.id}`}>
-            <h3>{palette.paletteName}</h3>
+          <Link key={palette.id} to={`palette/${palette.id}`}>
+            <MiniPalette {...palette} />
           </Link>
         ))}
       </div>
