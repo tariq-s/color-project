86e0c60cabfc2ccb974a10555e062f78921e64ad
Tariq <tariqshariff.ts@gmail.com>
2021-12-22 10:35:32 +0530

    add colorHelper to generate color shades

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./Palette.css";
 import ColorBox from "./ColorBox";
+import { generatePalette } from "./colorHelpers";
 
 export default class Palette extends Component {
   render() {
+    console.log(generatePalette(this.props.palette));
     const colorBoxes = this.props.palette.colors.map((color) => (
       <ColorBox background={color.color} name={color.name} />
     ));
-    console.log(colorBoxes);
     return (
       <div className="palette">
         <div className="palette-colors">{colorBoxes}</div>
