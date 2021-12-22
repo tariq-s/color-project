fc6cd236d2b9db13d23b7199b6bb3149a9bab131
Tariq <tariqshariff.ts@gmail.com>
2021-12-22 15:51:28 +0530

    finish styling mini palette

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -14,7 +14,11 @@ const styles = {
     },
   },
   colors: {
-    backgroundColor: "grey",
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden",
   },
   title: {
     display: "flex",
@@ -28,13 +32,28 @@ const styles = {
   emoji: {
     marginLeft: "0.5rem",
   },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-4px",
+  },
 };
 
 function MiniPalette(props) {
   const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      key={color.name}
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+    ></div>
+  ));
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
+      <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
