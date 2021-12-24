f2eb0ca17d40d6a43641c44256a23bd9b885cc3a
Tariq <tariqshariff.ts@gmail.com>
2021-12-24 17:16:45 +0530

    style draggeable color box

--- colors-app/src/DraggableColorBox.js
+++ colors-app/src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles = {
   root: {
@@ -10,13 +11,38 @@ const styles = {
     position: "relative",
     cursor: "pointer",
     marginBottom: "-5px",
+    "&:hover svg": {
+      color: "white",
+      transform: "scale(1.5)",
+    },
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out",
   },
 };
 
 function DraggableColorBox({ color, name, classes }) {
   return (
     <div className={classes.root} style={{ backgroundColor: color }}>
-      {name}
+      <div className={classes.boxContent}>
+        <span>{name}</span>
+        <span>
+          <DeleteIcon className={classes.deleteIcon} />
+        </span>
+      </div>
     </div>
   );
 }
