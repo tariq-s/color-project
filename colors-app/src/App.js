0730787e6006597564301e3ec0232774ac1d52c5
Tariq <tariqshariff.ts@gmail.com>
2021-12-26 11:18:12 +0530

    save palettes to loacl storage

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -9,15 +9,26 @@ import NewPaletteForm from "./NewPaletteForm";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
+    this.syncLocalStorage = this.syncLocalStorage.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find((palette) => palette.id === id);
   }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
   render() {
     return (
