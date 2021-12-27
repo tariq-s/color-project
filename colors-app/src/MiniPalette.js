89a8238ab71abc90dce4b5282cb783a0d1dc9f8e
Tariq <tariqshariff.ts@gmail.com>
2021-12-27 11:54:19 +0530

    refactor with pure component

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/MiniPaletteStyles";
 import Delete from "@material-ui/icons/Delete";
-class MiniPalette extends Component {
+class MiniPalette extends PureComponent {
   constructor(props) {
     super(props);
     this.removePalette = this.removePalette.bind(this);
@@ -12,7 +12,7 @@ class MiniPalette extends Component {
     this.props.openDialog(this.props.id);
   }
   render() {
-    const { classes, paletteName, emoji, colors, handleClick } = this.props;
+    const { classes, paletteName, emoji, colors, handleClick, id } = this.props;
     const miniColorBoxes = colors.map((color) => (
       <div
         key={color.name}
@@ -21,7 +21,7 @@ class MiniPalette extends Component {
       />
     ));
     return (
-      <div className={classes.root} onClick={handleClick}>
+      <div className={classes.root} onClick={() => handleClick(id)}>
         <div className={classes.delete}>
           <Delete
             className={classes.deleteIcon}
